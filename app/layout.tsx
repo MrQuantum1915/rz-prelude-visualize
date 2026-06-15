2ebe0fe1a38113ff24bf6dee5da7bea9d1c20d1c
Mr.Quantum_1915 <darshanpatelgdh@gmail.com>
2026-06-15 16:04:52 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Prelude Tree Visualiser",
+  description: "Visualise for prelude tree genereted by prologue-gen plugin in  Rizin",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
